8102d71e21c29061e31b017ce6e68ae88acc3920
imgjeits <imgjeits@stud.ntnu.no>
2021-02-28 18:59:44 +0100

    creation of acctNum, dob and creation date to DB

--- routes/customers.js
+++ routes/customers.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { createCollection } = require('../models/customer')
 const router = express.Router()
 const Customer = require('../models/customer')
 
@@ -13,7 +14,6 @@ router.get('/', async (req, res) => {
 
 // Check if customer exists in database
 router.get('/find', async (req, res) => {
-    console.log(req, res)
     try {
         const customers = await Customer.find({ personal_number: req.query.customerCheck })
         console.log(customers)
@@ -51,10 +51,49 @@ router.get('/new', (req, res) => {
 
 // Create new customer
 router.post('/', async (req, res) => {
+    console.log(req.body)
+
+    // Create random account number
+    const bankReg = '1234'
+    const typeOfAcct = req.body.acctType
+    let customerNo = Math.floor(Math.random() * 10000).toString()
+    let beforeControlNum = bankReg + typeOfAcct + customerNo
+
+    // Get control number for account with mod11
+    function mod11(account) {
+        const weights = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+        sum = 0
+        for (let i = 0; i < account.length; i++) {
+            sum += Number(account[i]) * weights[i]
+        }
+        const ctrlNum = (11 - (sum % 11))
+        if (ctrlNum == 11) {
+            return '0'
+        } else if (ctrlNum == 10) {
+            customerNo = Math.floor(Math.random() * 10000).toString()
+            beforeControlNum = bankReg + typeOfAcct + customerNo
+            return mod11(beforeControlNum)
+        } else {
+            return ctrlNum
+        }
+    }
+    const accountNumber = beforeControlNum + mod11(beforeControlNum).toString()
+
+    // Set birthdate
+    const pno = req.body.pno
+    const date = pno.toString().substr(0, 6) //140593
+    const day = date.substr(0, 2)
+    const month = date.substr(2, 2)
+    const year = date.substr(4, 2) > 21 ? `19${date.substr(4, 2)}` : `20${date.substr(4, 2)}`
+    const birthdate = new Date(`${year}-${month}-${day}`)
+
+    // Create customer object
     const customer = new Customer({
         personal_number: req.body.pno,
+        account_number: accountNumber,
         first_name: req.body.fName,
         last_name: req.body.lName,
+        date_of_birth: birthdate,
         city: req.body.city
     })
 
