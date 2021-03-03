e8a348dc4c9a2ae42b9fe8b9427cb3642aeb8463
imgjeits <imgjeits@stud.ntnu.no>
2021-03-03 20:39:48 +0100

    added methodOverride

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 // Routers
 const indexRouter = require('./routes/index')
@@ -17,10 +18,11 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //Throws warning
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.on('open', () => console.error('Connected to Mongoose'))
