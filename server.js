51be7fb36967d7da5f392a9dcdaff1844a9c5caa
Ida <imgjeits@stud.ntnu.no>
2021-03-21 20:38:38 +0100

    updated packages and server

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 // Routers
@@ -19,17 +18,28 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// Database connection
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
 const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.on('open', () => console.error('Connected to Mongoose'))
+db.on('error', error => console.error('❌ Mongoose DB connection\n', error))
+db.on('open', () => console.log('✅ Mongoose DB connection'))
 
 // Routes
 app.use('/', indexRouter)
 app.use('/customers', customerRouter)
 
-// Server
-app.listen(process.env.PORT || 3000)
+// Error handling
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.json({ error: `${err}` })
+})
 
+// Server
+app.listen(process.env.PORT || 3000, () => console.log(`✅ Server running [👂:${process.env.PORT}]`))
